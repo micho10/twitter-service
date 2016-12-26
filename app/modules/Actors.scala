c90a8c0c67cb9e515208e653bb78254a45816169
micho <gatin13@yahoo.com>
2016-12-26 18:12:37 +0900

    Added an actor module

---
+++ app/modules/Actors.scala
@@ -0,0 +1,40 @@
+package modules
+
+import javax.inject.Inject
+
+import actors.StatisticsProvider
+import akka.actor.ActorSystem
+import com.google.inject.AbstractModule
+
+/**
+  * Created by carlos on 26/12/16.
+  *
+  * Injects the ActorSystem in the module implementation so it can create actors.
+  */
+class Actors @Inject() (system: ActorSystem) extends ApplicationActors {
+  system.actorOf(
+    props = StatisticsProvider.props,
+    name = "statisticsProvider"
+  )
+}
+
+
+/**
+  * Defines a marker trait for the actor's module
+  */
+trait ApplicationActors
+
+
+/**
+  * Implements the actor's Guice module
+  */
+class ActorsModule extends AbstractModule {
+
+  override def configure(): Unit = {
+    // Defines the binding as eager, so it's initialized when the application is wired up and is available to any
+    // component in the app without explicitly depending on it.
+    bind(classOf[ApplicationActors])
+      .to(classOf[Actors]).asEagerSingleton()
+  }
+
+}
