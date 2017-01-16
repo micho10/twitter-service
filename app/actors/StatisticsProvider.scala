17b06e421b9a0fd6da3d0ab7d23c12047ca69ad2
micho <gatin13@yahoo.com>
2017-01-16 16:29:41 +0900

    Added specialized supervision to the Storage child actor

--- app/actors/StatisticsProvider.scala
+++ app/actors/StatisticsProvider.scala
@@ -1,8 +1,10 @@
 package actors
 
+import actors.StatisticsProvider.{ReviveStorage, ServiceUnavailable}
 import akka.actor.SupervisorStrategy.{Escalate, Restart}
-import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, Props, SupervisorStrategy}
+import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, Props, SupervisorStrategy, Terminated}
 import akka.stream.ConnectionException
+import messages.ComputeReach
 
 /**
   * Created by carlos on 26/12/16.
@@ -15,12 +17,6 @@ class StatisticsProvider extends Actor with ActorLogging {
   var storage: ActorRef = _
   var followersCounter: ActorRef = _
 
-  // Implements the receive method, which is the only method an actor needs to implement
-  override def receive = {
-    // Handles any kind of incoming message by literally doing nothing
-    case message => // do nothing
-  }
-
   /**
     * Child actors must be created inside this method of the parent actor to ensure that they'll be re-created
     * if the parent actor crashes. When a parent crashes all children are terminated as well.
@@ -32,10 +28,36 @@ class StatisticsProvider extends Actor with ActorLogging {
     // All children are accessible through the "context.children" collection,
     // or by their name with the "context.child(childName)" method.
     followersCounter = context.actorOf(Props[UserFollowersCounter], name = "serFollowersCounter")
-
     storage = context.actorOf(Props[Storage], name = "storage")
-
     reachComputer = context.actorOf(TweetReachComputer.props(followersCounter, storage), name = "tweetReachComputer")
+
+    // Registers to the lifecycle monitoring of the Storage child actor
+    context.watch(storage)
+  }
+
+  // Implements the receive method, which is the only method an actor needs to implement
+  override def receive = {
+    // Handles any kind of incoming message by literally doing nothing
+    case reach: ComputeReach =>
+      reachComputer forward reach
+    // Reacts to the termination. Because you only subscribed to the Storage actor notifications,
+    // it must be that actor that terminated
+    case Terminated(terminatedStorageRef) =>
+      // Schedules to send ifself a ReviveStorage message after a minute
+      context.system.scheduler.scheduleOnce(1.minute, self, ReviveStorage)
+      // Switches to a newly defined behaviour
+      context.become(storageUnavailable)
+  }
+
+  def storageUnavailable: Receive = {
+    case ComputeReach(_) =>
+      // Responds to ComputeReach requests by telling the client the service is unavailable
+      sender() ! ServiceUnavailable
+    case ReviveStorage =>
+      // Revives the Storage child actor
+      storage = context.actorOf(Props[Storage], name = "storage")
+      // Switches back to the original behaviour
+      context.unbecome()
   }
 
   override def unhandled(message: Any): Unit = {
@@ -55,8 +77,6 @@ class StatisticsProvider extends Actor with ActorLogging {
         super.supervisorStrategy.decider.applyOrElse(t, _ => Escalate)
     }
 
-
-
 }
 
 
@@ -64,5 +84,9 @@ class StatisticsProvider extends Actor with ActorLogging {
   * Defines how the actor can be instantiated by providing the Actor's Props
   */
 object StatisticsProvider {
+
+  case object ServiceUnavailable
+  case object ReviveStorage
+
   def props = Props[StatisticsProvider]
 }
