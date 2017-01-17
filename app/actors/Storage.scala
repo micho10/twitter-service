095a2d1ee16240c525fc452272dce45710ac0cf3
micho <gatin13@yahoo.com>
2017-01-17 12:20:37 +0900

    Added a singleton implementation of StoredReach

--- app/actors/Storage.scala
+++ app/actors/Storage.scala
@@ -1,10 +1,50 @@
 package actors
 
+import actors.storage.LastStorageError
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.stream.ConnectionException
 import messages.{ReachStored, StoreReach}
 import org.joda.time.DateTime
 //import reactivemongo.
+
+
+case class StoredReach(when: DateTime, tweetItd: BigInt, score: Int)
+
+
+object StoredReach {
+
+  implicit object BigIntHandler extends BSONDocumentReader[BigInt] with BSONDocumentWriter[BigInt] {
+    def write(bigInt: BigInt): BSONDocument = BSONDocument(
+      "signum"  -> bigInt.signum,
+      "value"   -> BSONBinary(BigInt.toByteArray, Subtype.UserDefinedSubtype)
+    )
+
+    def read(doc: BSONDocument): BigInt = BigInt(
+      doc.getAs[Int]("signum").get, {
+        val buf = doc.getAs[BSONBinary]("value").get.value
+        buf.readArray(buf.readable())
+      })
+  }
+
+  implicit object StoredReachHandler extends BSONDocumentReader[StoredReach] with BSONDocumentWriter[StoredReach] {
+    override def read(bson: BSONDocument): StoredReach = {
+      val when = bson.getAs[BSONDateTime]("when").map(t => new DateTime(t.value)).get
+      val tweetId = bson.getAs[BigInt]("tweet_id").get
+      val score = bson.getAs[BigInt]("score").get
+      StoredReach(when, tweetId, score)
+    }
+
+    override def write(r: StoredReach): BSONDocument = BSONDocument(
+      "when"      -> BSONDateTime(r.when.getMillis),
+      "tweetId"   -> r.tweetItd,
+      "tweet_id"  -> r.tweetItd,
+      "score"     -> r.score
+    )
+  }
+
+}
+
+
 
 /**
   * Created by carlos on 27/12/16.
@@ -21,9 +61,6 @@ class Storage extends Actor with ActorLogging {
   var db: DefaultDB = _
   var collection: BSONCollection = _
   obtainConnection()
-
-  // Keeps track of the identifiers you're currently trying to write
-  var currentWrites = Set.empty[BigInt]
 
 
   /**
@@ -50,6 +87,9 @@ class Storage extends Actor with ActorLogging {
     driver.close()
   }
 
+
+  // Keeps track of the identifiers you're currently trying to write
+  var currentWrites = Set.empty[BigInt]
 
   override def receive = {
     case StoreReach(tweetId, score) =>
@@ -92,10 +132,6 @@ class Storage extends Actor with ActorLogging {
   }
 
 }
-
-
-
-case class StoredReach(when: DateTime, tweetItd: BigInt, score: Int)
 
 
 
