3ec511593e844cdcad8fdf9e95835e6f4c8a4421
micho <gatin13@yahoo.com>
2017-01-17 14:54:55 +0900

    Retrieve the user followers count and check limits

--- app/actors/UserFollowersCounter.scala
+++ app/actors/UserFollowersCounter.scala
@@ -1,24 +1,75 @@
 package actors
 
 import akka.actor.{Actor, ActorLogging, Props}
+import akka.dispatch.ControlMessage
+import messages.FollowerCount
+import org.joda.time.DateTime
+import play.api.libs.oauth.OAuthCalculator
+import play.api.libs.ws.WS
+
+import scala.concurrent.Future
+
 
 /**
   * Created by carlos on 27/12/16.
   */
-class UserFollowersCounter extends Actor with ActorLogging {
+class UserFollowersCounter extends Actor with ActorLogging with TwitterCredentials {
 
   override def receive = {
     case message => // do nothing
   }
+
 
   override def unhandled(message: Any): Unit = {
     log.warning("Unhandled message {} message from {}", message, sender())
     super.unhandled(message)
   }
 
+
+  val LimitRemaining  = "X-Rate-Limit-Remaining"
+  val LimitReset      = "X-Rate-Limit-Reset"
+
+
+  private def fetchFollowerCount(tweetId: BigInt, userId: BigInt): Future[FollowerCount] = {
+    credentials.map {
+      case (consumerKey, requestToken) =>
+        WS.url("https://api.twitter.com/1.1/users/show.json")
+          .sign(OAuthCalculator(consumerKey, requestToken))
+          .withQueryString("user_id" -> userId.toString)
+          .get().map { response =>
+            if (response.status == 200) {
+
+              val rateLimit = for {
+                remaining <- response.header(LimitRemaining)
+                reset     <- response.header(LimitReset)
+              } yield (remaining.toInt, new DateTime(reset.toLong * 1000))
+
+              rateLimit.foreach { case (remaining, reset) =>
+                log.info("Rate limit: {} requestes remaining, window resets at {}", remaining, reset)
+                if (remaining < 50) Thread.sleep(10000)
+                if (remaining < 10) context.parent ! TwitterRateLimitReached(reset)
+              }
+
+              val count = (response.json \ "followers_count").as[Int]
+              FollowerCount(tweetId, userId, count)
+            } else throw new RuntimeException(s"Could not retrieve followers count for user $userId")
+        }
+    }.getOrElse {
+      Future.failed(new RuntimeException("You did not configure the Twitter credentials correctly"))
+    }
+  }
+
 }
+
 
 
 object userFollowersCounter {
   def props = Props[UserFollowersCounter]
 }
+
+
+
+case class TwitterRateLimitReached(reset: DateTime) extends ControlMessage
+case class FollowerCountUnavailable(tweetId: BigInt, user: BigInt)
+case object UserFollowersCounterUnavailable extends ControlMessage
+case object UserFollowersCounterAvailable extends ControlMessage
